9e86f328b2903cefa2c8b983d005f9765818d661
Fernando Ribeiro <fribeiro@ibm.com>
2023-05-30 14:26:41 -0300

    Fix escapable character.

--- src/components/home/home-initial.tsx
+++ src/components/home/home-initial.tsx
@@ -7,13 +7,13 @@ export default function HomeInitial() {
         <div>
           <div className="flex">
             <img className="rounded-full display: inline w-1/3" src={profilePicture.src} alt="Profile picture"/>
-            <span className="pl-1 pt-24 text-xl">Hi I'm Fernando Ribeiro</span>
+            <span className="pl-1 pt-24 text-xl">Hi Im Fernando Ribeiro</span>
           </div>
           <p className="pl-2">and this is my development features showcase!</p>
         </div>
       </div>
       <span className="flex items-center justify-center">
-        <svg className="h-20 w-20 animate-bounce" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg className="h-20 w-20 animate-bounce" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M12 5v14m-7-7l7 7 7-7"/>
         </svg>
       </span>
